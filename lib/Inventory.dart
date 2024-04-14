06daf7b22a049eff5ff98997ca49a07489210666
Aaron <aaronaaronmathias@gmail.com>
2024-04-14 15:43:43 +0530

    code and list displaying

--- lib/Inventory.dart
+++ lib/Inventory.dart
@@ -1,38 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Inventory App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Inventory(),
-    );
-  }
-}
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FirebaseTodo {
-  final String? category;
   late CollectionReference _todosCollection;
 
-  FirebaseTodo({required this.category}) {
-    if (category != null) {
-      _todosCollection = FirebaseFirestore.instance
-          .collection('sharedCollection')
-          .doc('405898')
-          .collection(category!);
-    }
+  FirebaseTodo({required String category, required String globalConnectCode}) {
+    _todosCollection = FirebaseFirestore.instance
+        .collection('sharedCollection')
+        .doc(globalConnectCode)
+        .collection(category);
   }
 
   Future<void> addTodo({
@@ -41,9 +18,9 @@ class FirebaseTodo {
     required String weight,
   }) async {
     await _todosCollection.doc(title).set({
+      'title': title,
       'quantity': quantity,
       'weight': weight,
-      'category': category,
     });
   }
 
@@ -68,191 +45,6 @@ class FirebaseTodo {
 
   Stream<QuerySnapshot> getTodos() {
     return _todosCollection.snapshots();
-  }
-}
-
-class Inventory extends StatefulWidget {
-  const Inventory({Key? key}) : super(key: key);
-
-  @override
-  State<Inventory> createState() => _InventoryState();
-}
-
-class _InventoryState extends State<Inventory> {
-  late TextEditingController _titleController;
-  late TextEditingController _quantityController;
-  late TextEditingController _weightController;
-  late FirebaseTodo firebaseTodo;
-  String? selectedCategory;
-  List<bool> _isOpenList = [false, false, false, false, false]; // Keeps track of expansion state
-  List<int> _expandedIndices = [-1]; // Track currently expanded panel index
-
-  @override
-  void initState() {
-    super.initState();
-    selectedCategory = 'Fruits';
-    firebaseTodo = FirebaseTodo(category: selectedCategory);
-    _titleController = TextEditingController();
-    _quantityController = TextEditingController();
-    _weightController = TextEditingController();
-  }
-
-  void _addItem() {
-    String title = _titleController.text;
-    firebaseTodo.addTodo(
-      title: title,
-      quantity: _quantityController.text,
-      weight: _weightController.text,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Inventory'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            _buildExpandableSection(
-              title: 'Fruits',
-              content: FirebaseTodoList(firebaseTodo: FirebaseTodo(category: 'fruits')),
-              index: 0,
-            ),
-            _buildExpandableSection(
-              title: 'Vegetables',
-              content: FirebaseTodoList(firebaseTodo: FirebaseTodo(category: 'vegetables')),
-              index: 1,
-            ),
-            _buildExpandableSection(
-              title: 'Daily Essentials',
-              content: FirebaseTodoList(firebaseTodo: FirebaseTodo(category: 'daily essentials')),
-              index: 2,
-            ),
-            _buildExpandableSection(
-              title: 'Medicines',
-              content: FirebaseTodoList(firebaseTodo: FirebaseTodo(category: 'medicines')),
-              index: 3,
-            ),
-            _buildExpandableSection(
-              title: 'Pulses',
-              content: FirebaseTodoList(firebaseTodo: FirebaseTodo(category: 'pulses')),
-              index: 4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          _showAddDialog(context);
-        },
-        label: Text('Add'),
-        icon: Icon(Icons.add),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        backgroundColor: Colors.blue,
-        elevation: 2.0,
-        tooltip: 'Add a new item',
-        isExtended: true,
-        splashColor: Colors.blueAccent,
-      ),
-    );
-  }
-
-  Widget _buildExpandableSection({
-    required String title,
-    required Widget content,
-    required int index,
-  }) {
-    return ExpansionPanelList.radio(
-      elevation: 2,
-      expandedHeaderPadding: EdgeInsets.all(10),
-      children: [
-        ExpansionPanelRadio(
-          value: index,
-          headerBuilder: (context, isExpanded) {
-            return Container(
-              color: Colors.blue, // Set the background color here
-              child: ListTile(
-                title: Text(
-                  title,
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ),
-            );
-          },
-          body: content,
-          canTapOnHeader: true,
-        ),
-      ],
-      expansionCallback: (int item, bool isExpanded) {
-        setState(() {
-          _expandedIndices.clear(); // Clear previous expansion
-          if (!isExpanded) {
-            _expandedIndices.add(item);
-          }
-        });
-      },
-    );
-  }
-
-  Future<void> _showAddDialog(BuildContext context) async {
-    String? _chosenValue;
-
-    await showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Add Item'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _titleController,
-                decoration: InputDecoration(labelText: 'Item Name'),
-              ),
-              TextField(
-                controller: _quantityController,
-                decoration: InputDecoration(labelText: 'Quantity'),
-              ),
-              TextField(
-                controller: _weightController,
-                decoration: InputDecoration(labelText: 'Weight'),
-              ),
-              DropDownDemo(
-                onChanged: (String? value) {
-                  _chosenValue = value;
-                  setState(() {
-                    selectedCategory = value ?? 'Fruits';
-                    firebaseTodo = FirebaseTodo(category: selectedCategory);
-                  });
-                },
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _addItem();
-                _titleController.clear();
-                _quantityController.clear();
-                _weightController.clear();
-                Navigator.pop(context);
-              },
-              child: Text('Add'),
-            ),
-          ],
-        );
-      },
-    );
   }
 }
 
@@ -306,41 +98,215 @@ class FirebaseTodoList extends StatelessWidget {
   }
 }
 
-class DropDownDemo extends StatefulWidget {
-  final Function(String?) onChanged;
-
-  DropDownDemo({required this.onChanged});
+class Inventory extends StatefulWidget {
+  const Inventory({Key? key}) : super(key: key);
 
   @override
-  _DropDownDemoState createState() => _DropDownDemoState();
+  State<Inventory> createState() => _InventoryState();
 }
 
-class _DropDownDemoState extends State<DropDownDemo> {
-  String? _chosenValue;
+class _InventoryState extends State<Inventory> {
+  late TextEditingController _titleController;
+  late TextEditingController _quantityController;
+  late TextEditingController _weightController;
+  String? selectedCategory;
+  String? globalConnectCode;
+  FirebaseTodo? firebaseTodo;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedCategory = 'fruits';
+    _getGlobalConnectCode();
+    _titleController = TextEditingController();
+    _quantityController = TextEditingController();
+    _weightController = TextEditingController();
+  }
+
+  void _getGlobalConnectCode() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      globalConnectCode = prefs.getString('globalConnectCode');
+    });
+  }
+
+  void _addItem() {
+    String title = _titleController.text;
+    String quantity = _quantityController.text;
+    String weight = _weightController.text;
+    if (selectedCategory != null && globalConnectCode != null) {
+      firebaseTodo = FirebaseTodo(
+        category: selectedCategory!,
+        globalConnectCode: globalConnectCode!,
+      );
+      firebaseTodo!.addTodo(
+        title: title,
+        quantity: quantity,
+        weight: weight,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-        value: _chosenValue,
-        items: <String?>[
-          null,
-          'Fruits',
-          'Vegetables',
-          'Daily Essentials',
-          'Medicines',
-          'Pulses',
-        ].map<DropdownMenuItem<String>>((String? value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value ?? 'Select Category'),
-          );
-        }).toList(),
-        onChanged: (String? value) {
-          setState(() {
-            _chosenValue = value;
-            widget.onChanged(value);
-          });
-          },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inventory'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildExpandableSection(
+              title: 'Fruits',
+              content: FirebaseTodoList(
+                firebaseTodo: FirebaseTodo(
+                  category: 'fruits',
+                  globalConnectCode: globalConnectCode ?? '',
+                ),
+              ),
+            ),
+            _buildExpandableSection(
+              title: 'Vegetables',
+              content: FirebaseTodoList(
+                firebaseTodo: FirebaseTodo(
+                  category: 'vegetables',
+                  globalConnectCode: globalConnectCode ?? '',
+                ),
+              ),
+            ),
+            _buildExpandableSection(
+              title: 'Daily Essentials',
+              content: FirebaseTodoList(
+                firebaseTodo: FirebaseTodo(
+                  category: 'daily essentials',
+                  globalConnectCode: globalConnectCode ?? '',
+                ),
+              ),
+            ),
+            _buildExpandableSection(
+              title: 'Medicines',
+              content: FirebaseTodoList(
+                firebaseTodo: FirebaseTodo(
+                  category: 'medicines',
+                  globalConnectCode: globalConnectCode ?? '',
+                ),
+              ),
+            ),
+            _buildExpandableSection(
+              title: 'Pulses',
+              content: FirebaseTodoList(
+                firebaseTodo: FirebaseTodo(
+                  category: 'pulses',
+                  globalConnectCode: globalConnectCode ?? '',
+                ),
+              ),
+            ),
+            Text(
+              'Global Connect Code: ${globalConnectCode ?? "Not Available"}',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          _showAddDialog(context);
+        },
+        label: Text('Add'),
+        icon: Icon(Icons.add),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        backgroundColor: Colors.blue,
+        elevation: 2.0,
+        tooltip: 'Add a new item',
+        isExtended: true,
+        splashColor: Colors.blueAccent,
+      ),
+    );
+  }
+
+  Widget _buildExpandableSection({
+    required String title,
+    required Widget content,
+  }) {
+    return ExpansionTile(
+      title: Text(
+        title,
+        style: TextStyle(fontSize: 20.0),
+      ),
+      children: [
+        content,
+      ],
+    );
+  }
+
+  Future<void> _showAddDialog(BuildContext context) async {
+    String? _chosenValue;
+
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add Item'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              DropdownButton<String>(
+                value: selectedCategory,
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedCategory = value;
+                  });
+                },
+                items: <String>[
+                  'fruits',
+                  'vegetables',
+                  'daily essentials',
+                  'medicines',
+                  'pulses'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(labelText: 'Item Name'),
+              ),
+              TextField(
+                controller: _quantityController,
+                decoration: InputDecoration(labelText: 'Quantity'),
+              ),
+              TextField(
+                controller: _weightController,
+                decoration: InputDecoration(labelText: 'Weight'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _addItem();
+                _titleController.clear();
+                _quantityController.clear();
+                _weightController.clear();
+                Navigator.pop(context);
+              },
+              child: Text('Add'),
+            ),
+          ],
         );
-    }
+      },
+    );
+  }
 }
+
